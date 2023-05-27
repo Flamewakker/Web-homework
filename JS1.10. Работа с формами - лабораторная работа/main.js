49ee86c79fb655a65574c4d93166e688f8ebab4e
Flamewakker <95296187+Flamewakker@users.noreply.github.com>
2023-05-27 10:38:40 +0300

    .

--- JS1.10. Работа с формами - лабораторная работа/main.js
+++ JS1.10. Работа с формами - лабораторная работа/main.js
@@ -4,6 +4,6 @@ const passwordInput = document.querySelector('#password');
 form.addEventListener('submit', (event) => {
   if (passwordInput.value.length < 8) {
     event.preventDefault();
-    alert('Password must contain at least 8 characters');
+    alert('Пароль должен содержать не менее 8 символов');
   }
 });
